82dd3f115e472e65e814791cfc1dbfecd3adc3b7
Dimas Capelari <dimas.capelari@gmail.com>
2022-09-01 14:02:40 -0300

    257 - configurando parametros de rotas

--- src/router.js
+++ src/router.js
@@ -12,7 +12,7 @@ export default new Router({
         path: '/',
         component: Inicio
     }, {
-        path: '/usuario',
+        path: '/usuario/:id',
         component: Usuario
     }]
 })
